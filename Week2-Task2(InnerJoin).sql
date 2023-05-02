7a99b5c8df9e92791921ba2122280fa055329329
Jon Aldrete <98139941+J272881@users.noreply.github.com>
2023-05-01 22:02:29 -0700

    Update Week2-Task2(InnerJoin).sql

--- Week2-Task2(InnerJoin).sql
+++ Week2-Task2(InnerJoin).sql
@@ -1,7 +1,7 @@
 -- Task 2: Join
 SELECT c.CustomerID, 
 CONCAT(c.CustomerFirstName, ' ', c.CustomerLastName ) AS CustomerFullName,
-b.BookingID, o.orderID, o.TotalCost, m.Cuisine AS MenuName, i.ItemName, i.ItemType
+ o.OrderID, o.TotalCost, m.Cuisine AS MenuName, i.ItemName, i.ItemType
 FROM customers AS c, bookings AS b, orders AS o, menus as m, menuitems as i
 WHERE c.CustomerID = b.CustomerID
 AND b.BookingID = o.BookingID
